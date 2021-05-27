49a73f1434c01cee5b29f24e97b1a8fd231e490c
Reymond <70185846+rgomera@users.noreply.github.com>
2021-05-27 15:32:51 +0800

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -21,7 +21,7 @@ const Login = () => {
         e.preventDefault();
         try {
             const body = { email, password };
-            const response = await fetch('http://localhost:5000/auth/login', {
+            const response = await fetch(`${process.env.PORT}/auth/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(body),
